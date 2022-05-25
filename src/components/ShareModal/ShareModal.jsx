afdb82b4ff970af998cea551e65f64f9ac8e604e
Muhammad Asif <asif88383@gmail.com>
2022-05-25 21:03:03 +0500

    first commit

--- src/components/ShareModal/ShareModal.jsx
+++ src/components/ShareModal/ShareModal.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Modal, useMantineTheme } from "@mantine/core";
+import PostShare from "../PostShare/PostShare";
 
-const ShareModal = () => {
+const ShareModal = ({ modalOpened, setModalOpened }) => {
+  const theme = useMantineTheme();
+
   return (
-    <div>ShareModal</div>
-  )
-}
+    <Modal
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.light[0]
+      }
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      size="55"
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+    >
+      <PostShare />
+    </Modal>
+  );
+};
 
-export default ShareModal
+export default ShareModal;
