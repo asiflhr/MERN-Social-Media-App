8e021356a2b67475840106ad4e0d6b2ad8c78353
Muhammad Asif <asif88383@gmail.com>
2022-05-25 12:09:28 +0500

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="profileSide"></div>
+      <div className="postSide"></div>
+      <div className="RightSide"></div>
+    </div>
+  );
+};
+
+export default Home;
